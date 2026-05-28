faabaab0598b86bf9ae5e72d29fd85c6511f27c5
venomous-maker <morganokumu8+1@gmail.com>
2026-05-29 01:08:29 +0300

    Update HorizonServiceProvider

--- packages/horizon/src/HorizonServiceProvider.ts
+++ packages/horizon/src/HorizonServiceProvider.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { ServiceProvider } from "@lara-node/core";
+import { ServiceProvider, type CommandClass } from "@lara-node/core";
+import {
+  HorizonWorkCommand, HorizonPauseCommand, HorizonContinueCommand,
+  HorizonTerminateCommand, HorizonStatusCommand, HorizonListCommand,
+} from "./Commands.js";
 import { horizonMetrics } from "./HorizonMetrics.js";
 import { HorizonManager } from "./HorizonManager.js";
 import { renderHorizonDashboard } from "./HorizonDashboard.js";
@@ -22,6 +26,10 @@ import horizonConfig from "./horizon.config.js";
 
 export class HorizonServiceProvider extends ServiceProvider {
   register(): void {}
+
+  commands(): CommandClass[] {
+    return [HorizonWorkCommand, HorizonPauseCommand, HorizonContinueCommand, HorizonTerminateCommand, HorizonStatusCommand, HorizonListCommand];
+  }
 
   async boot(): Promise<void> {
     if (!this.isEnabled()) return;
